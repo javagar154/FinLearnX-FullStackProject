63dbdb4670a3075e6a052108fefcd5c0f6dec273
Javagar R <javagarr85@gmail.com>
2026-06-29 18:36:20 +0530

    Update HomeController.java

--- backend/src/main/java/com/finlearnx/controller/HomeController.java
+++ backend/src/main/java/com/finlearnx/controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-```
+
 @GetMapping("/")
 public String home() {
     return "FinLearnX Backend is Running 🚀";
 }
-```
+
 
 }
